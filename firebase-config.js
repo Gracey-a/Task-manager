cbe6496980751ee6297e50370f71b621cc05e7f5
Agho Ivie Gracious <ivieagho29@gmail.com>
2026-04-26 15:34:13 +0100

    Update Firebase configuration and imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,8 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDwC-rhXq240NHd1iQUjBfUzb1j6q8lV9w",
   authDomain: "taskforce-pro-2e653.firebaseapp.com",
@@ -9,3 +12,8 @@ const firebaseConfig = {
   appId: "1:577638265464:web:6e3e903c40617e8cdfb38d",
   measurementId: "G-9CBWBFTBEW"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
