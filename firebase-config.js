047ba454d42107e9113713c75eef2fb917307174
Agho Ivie Gracious <ivieagho29@gmail.com>
2026-04-26 15:32:59 +0100

    Add Firebase configuration file
    Added Firebase configuration for the web app.

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwC-rhXq240NHd1iQUjBfUzb1j6q8lV9w",
+  authDomain: "taskforce-pro-2e653.firebaseapp.com",
+  projectId: "taskforce-pro-2e653",
+  storageBucket: "taskforce-pro-2e653.firebasestorage.app",
+  messagingSenderId: "577638265464",
+  appId: "1:577638265464:web:6e3e903c40617e8cdfb38d",
+  measurementId: "G-9CBWBFTBEW"
+};
