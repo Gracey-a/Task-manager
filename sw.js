2791a40f32562f8865532522489faaa6b5eb2d8c
Agho Ivie Gracious <ivieagho29@gmail.com>
2026-04-26 15:57:24 +0100

    Update service worker cache version and URLs

--- sw.js
+++ sw.js
@@ -1,25 +1,15 @@
-const CACHE_NAME = "taskforce-v1";
+const CACHE_NAME = "taskforce-v2";
 const urlsToCache = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/js/main.js",
-    "/js/db.js",
-    "/js/utils.js",
-    "/js/ui.js",
-    "/js/reminders.js",
-    "/js/pomodoro.js",
-    "/manifest.json"
+    "/", "/index.html", "/styles.css", "/manifest.json",
+    "/js/main.js", "/js/db.js", "/js/ui.js", "/js/utils.js",
+    "/js/reminders.js", "/js/pomodoro.js", "/js/firebase.js",
+    "/js/kanban.js", "/js/analytics.js", "/js/swipe.js", "/js/shortcuts.js",
+    "/firebase-config.js"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
