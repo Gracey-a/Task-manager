6dda8d4368a1f8f29c6eda27384107365eaebfc5
Agho Ivie Gracious <ivieagho29@gmail.com>
2026-04-26 15:51:03 +0100

    Add swipe gesture initialization function

---
+++ js/swipe.js
@@ -0,0 +1,12 @@
+export function initSwipe(element, onSwipeLeft, onSwipeRight) {
+    let touchStartX = 0, touchEndX = 0;
+    element.addEventListener('touchstart', (e) => { touchStartX = e.changedTouches[0].screenX; });
+    element.addEventListener('touchend', (e) => {
+        touchEndX = e.changedTouches[0].screenX;
+        const diff = touchEndX - touchStartX;
+        if (Math.abs(diff) > 50) {
+            if (diff > 0) onSwipeRight();
+            else onSwipeLeft();
+        }
+    });
+}
