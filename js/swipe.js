34a4c2414b200dda52708cc5f94896aeb48e710e
Agho Ivie Gracious <ivieagho29@gmail.com>
2026-04-26 16:01:25 +0100

    Simplify swipe detection logic

--- js/swipe.js
+++ js/swipe.js
@@ -4,9 +4,6 @@ export function initSwipe(element, onSwipeLeft, onSwipeRight) {
     element.addEventListener('touchend', (e) => {
         touchEndX = e.changedTouches[0].screenX;
         const diff = touchEndX - touchStartX;
-        if (Math.abs(diff) > 50) {
-            if (diff > 0) onSwipeRight();
-            else onSwipeLeft();
-        }
+        if (Math.abs(diff) > 50) diff > 0 ? onSwipeRight() : onSwipeLeft();
     });
 }
